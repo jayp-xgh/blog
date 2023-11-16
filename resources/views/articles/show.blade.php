ae96079d43b6a1defca35000c1654d2891c5b5c1
jayp-zip <joaovitorpereiramartins.2004@gmail.com>
2023-11-16 20:09:35 -0300

    update: the theme colors of the articles have been changed

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,42 +10,63 @@
     <title>Artigos</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+        :root {
+            --main-font: 'Merriweather', serif;
+            --strong-color: #e96b32;
+            --italic-color: #92c0dd;
+            --heading-color: #2f85f3;
+            --heading-h2-h3-color: #71db8c;
+            --paragraph-color: #768390;
+            --background-color: #22272e;
+            --text-color: #ffffff;
+        }
+
+
         body {
             font-family: 'Roboto Mono', monospace;
         }
+        body, main{
+            background-color: var(--background-color);
+        }
+
         strong {
-            color: #619ce5;
-            font-size: 1.5rem;
+            color: var(--strong-color);
+            font-size: 1.3rem;
+            line-height: 1.2;
         }
+
         i {
-            color: rgb(136, 193, 234);
+            color: var(--italic-color);
         }
-        h2, h3, p {
-            padding-bottom: 15px; 
-        }
+
         h1 {
             font-size: 2.25rem;
             font-weight: 800;
-            color:#ffffff;
-            padding-bottom:56px;
+            color: var(--heading-color);
             line-height: 1.5;
         }
+
         h2, h3 {
             font-size: 1.5rem;
             font-weight: 700;
-            color: #ffffff;
+            color: var(--heading-h2-h3-color);
         }
-        p {
+
+        p, a{
             font-size: 1.5rem;
-            color: #676e8a;
+            color: var(--paragraph-color);
             line-height: 1.2;
         }
+        h1,h2,h3,h4,h5,h6,p {
+            padding-bottom: 10px;
+        }
+
     </style>
 </head>
 
-<body class="min-h-[100vh]  dark:bg-gray-900">
+<body class="min-h-[100vh]">
     <x-go-back-button />
-    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 dark:bg-gray-900 antialiased flex flex-col justify-center items-center">
+    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 antialiased flex flex-col justify-center items-center">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl w-[100%]">
             <article
                 class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
@@ -54,7 +75,7 @@
                         <div class="mr-3 text-sm text-gray-900 dark:text-white flex justify-center items-center">
                             <img class="mr-4 w-16 h-16 rounded-full" src="/img/jayp.png" alt="João Pereira">
                             <div>
-                                <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">João Pereira</a>
+                                <a href="../../" rel="author" class="text-xl font-bold ">João Pereira</a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">Desenvolvedor, Cientista da Computação & Entusiasta</p>
                             </div>
                         </div>
